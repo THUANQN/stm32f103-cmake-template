1480610cf4047300fe4b171548426c1a5d54a5cb
THUANQN <thuannguyenquang02081989@gmail.com>
2021-02-07 02:28:18 +0000

    second commit

---
+++ source/src_stdperiph/main.c
@@ -0,0 +1,122 @@
+/**
+ * This Example is using the mod_led.h in noarch_c_lib
+ * to toggle the LED. The difference with the simple
+ * example is that now the LED supports also patterns.
+ */
+
+#include <stdio.h>
+#include "stm32f10x.h"
+#include "debug_trace.h"
+#include "mod_led.h"
+#include "timer_sched.h"
+
+#if defined(USE_STTERM)
+#include "stlinky.h"
+#endif
+
+#define LED_TIMER_MS 500
+
+volatile uint32_t glb_tmr_1ms;
+
+#define LED_PORT GPIOC
+#define LED_PIN GPIO_Pin_13
+
+uint32_t trace_levels;
+
+/* Create the list head for the timer */
+static LIST_HEAD(obj_timer_list);
+
+// Declare uart
+#if defined(USE_DBGUART)
+#include "dev_uart.h"
+DECLARE_UART_DEV(dbg_uart, USART1, 115200, 256, 10, 1);
+#endif
+
+#if defined(USE_SEMIHOSTING)
+extern void initialise_monitor_handles(void);
+#endif
+
+#if defined(USE_OVERCLOCK)
+/* This function overclocks stm32 to 128MHz */
+extern uint32_t overclock_stm32f103(void);
+#endif
+
+static inline void main_loop(void)
+{
+	/* 1 ms timer */
+	if (glb_tmr_1ms) {
+		glb_tmr_1ms = 0;
+		mod_timer_polling(&obj_timer_list);
+	}
+}
+
+void led_on(void *data)
+{
+	LED_PORT->ODR |= LED_PIN;
+}
+
+void led_off(void *data)
+{
+	LED_PORT->ODR &= ~LED_PIN;
+}
+
+void led_init(void *data)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_Pin = LED_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(LED_PORT, &GPIO_InitStructure);
+
+	LED_PORT->ODR |= LED_PIN;
+	TRACE(("init\n"));
+}
+
+int main(void)
+{
+
+	#if defined(USE_OVERCLOCK)
+		/* overclock. Comment out for default clocks */
+		SystemCoreClock = overclock_stm32f103();
+	#endif
+
+	if (SysTick_Config(SystemCoreClock / 1000)) {
+		/* Capture error */
+		while (1);
+	}
+	
+	trace_levels_set(
+			0
+			| TRACE_LEVEL_DEFAULT
+			,1);
+
+#if defined(USE_SEMIHOSTING)
+	initialise_monitor_handles();
+#elif defined(USE_STTERM)
+	stlinky_init();
+#elif defined(USE_DBGUART)
+	// setup uart port
+	dev_uart_add(&dbg_uart);
+	// set callback for uart rx
+ 	dbg_uart.fp_dev_uart_cb = NULL;
+ 	mod_timer_add((void*) &dbg_uart, 5, (void*) &dev_uart_update, &obj_timer_list);
+#endif
+
+	/* Declare LED module and initialize it */
+	DECLARE_MODULE_LED(led_module, 8, 250);
+	mod_led_init(&led_module);
+	mod_timer_add((void*) &led_module, led_module.tick_ms, (void*) &mod_led_update, &obj_timer_list);
+
+	/* Declare LED */
+	DECLARE_DEV_LED(def_led, &led_module, 1, NULL, &led_init, &led_on, &led_off);
+	dev_led_add(&def_led);
+	dev_led_set_pattern(&def_led, 0b11001100);
+
+	TRACE(("Program started\n"));
+
+	/* main loop */
+	while (1) {
+		main_loop();
+	}
+}
